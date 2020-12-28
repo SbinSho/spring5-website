5c9f5e4ee9522b5344388cfce73fa009a935f360
SbinSho <suho0232@gmail.com>
2020-12-28 22:45:12 +0900

    spring5, mvc 초기 설정 완료

---
+++ src/main/java/config/MvcConfig.java
@@ -0,0 +1,28 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration // 해당 클래스를 스프링 설정 클래스로 설정
+@EnableWebMvc  // 스프링 MVC 설정을 활성화
+// WebMvcConfigurer : 스프링 MVC의 개별 설정을 조정할 떄 사용
+public class MvcConfig implements WebMvcConfigurer  { 
+
+	@Override
+	public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+		configurer.enable();
+		
+	}
+
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		registry.jsp("/WEB-INF/view/", ".jsp");
+		
+	}
+
+	
+	
+}
